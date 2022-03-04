14666613568c3e0149036fabed79859896a856c5
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-03-04 19:42:01 +0700

    ganti library

--- individual-project/src/App.js
+++ individual-project/src/App.js
@@ -1,22 +1,8 @@
 import './assets/styles.css';
-import { Grid } from '@mui/material';
-import Header from './component/Header/Header';
-import Content from './component/Content/Content';
 
 function App() {
   return (
-    <Grid spacing={2} container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid container justifyContent="space-between">
-        <Grid item xs={1} sm={2} />
-        <Grid item xs={10} sm={8}>
-          <Content />
-        </Grid>
-        <Grid item={1} sm={2} />
-      </Grid>
-    </Grid>
+
   );
 }
 
